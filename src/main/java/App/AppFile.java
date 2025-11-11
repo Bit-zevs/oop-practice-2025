61abd1bff2863b11803fe6461095e995598aa983
vanyaa322 <vanyaleks322@gmail.com>
2025-11-11 09:35:22 +0500

    Код обрабатывает соообщения и нажатия кнопок, отправляет ответы с клавиатуры.

---
+++ src/main/java/App/AppFile.java
@@ -0,0 +1,10 @@
+package App;
+
+import com.pengrad.telegrambot.TelegramBot;
+public class AppFile {
+    public static void main(String [] args) {
+        String TOKEN = "8312492716:AAG-h5O4vz1ShZwE-D3R8ED8hS8uTWvG2Hs";
+        TelegramBot bot = new TelegramBot(TOKEN);
+
+    }
+}
